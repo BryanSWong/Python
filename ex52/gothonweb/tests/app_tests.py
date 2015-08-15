77e9f28f1cf5571f621837953b425210735b5ac5
Bryan Wong <bwong727@gmail.com>
2015-08-15 00:40:53 -0400

    update ex 52

--- ex52/gothonweb/tests/app_tests.py
+++ ex52/gothonweb/tests/app_tests.py
@@ -4,9 +4,28 @@ from tests.tools import assert_response
 
 def test_index():
 	# check that we get a 404 on the / URL
+	resp = app.request("/game")
+	assert_response(resp, status="200")
 	resp = app.request("/")
-	assert_response(resp, status="404")
+	assert_response(resp, status="303")
+	
+def test_request_paths():
+	data = "action=tell+a+joke"
+	resp = app.request("/game", method="POST", data=data)
+	#print resp
+	assert_response(resp, status="303 See Other")
+	assert("webpy_session_id=" in resp.headers['Set-Cookie'])
 
+	resp = app.request("/hello")
+	#print resp
+	assert_response(resp, status="404 Not Found")
+
+	resp = app.request("/hello", method="POST")
+	#print resp
+	assert_response(resp, status="404 Not Found")
+	assert("webpy_session_id=" in resp.headers['Set-Cookie'])
+
+	'''
 	# test our first GET request to /hello
 	resp = app.request("/hello")
 	assert_response(resp)
@@ -19,3 +38,4 @@ def test_index():
 	data = {'name': 'Zed', 'greet': 'Hola'}
 	resp = app.request("/hello", method="POST", data=data)
 	assert_response(resp, contains="Zed")
+	'''
